32ae9713cdb6af7e0d522a2b47552e0e7aed5b5e
Psihopatch <75320638+Psihopatch@users.noreply.github.com>
2024-03-07 19:08:43 +0300

    Add files via upload

--- task3.1.py
+++ task3.1.py
@@ -2,13 +2,26 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv("students_data(1).csv")
-l = ["age", "Medu", "Fedu", "traveltime", "studytime", "failures", "famrel", "freetime", "goout", "Dalc", "Walc", "health", "absences", "G1", "G2", "G3"]
-lMean, lGMean, lMedian = dict(), dict(), dict()
-
-for i in l:
-    lMean[i] = data[i].mean()
-    lGMean[i] = np.sqrt(np.square(data[i]).mean())
-    lMedian[i] = np.median(data[i])
+n = ["age", "Medu", "Fedu", "traveltime", "studytime", "failures", "famrel", "freetime", "goout", "Dalc", "Walc", "health", "absences", "G1", "G2", "G3"]
+srt = ["Mjob", "Fjob", "reason", "guardian", "romantic", "cheating", "school", "sex", "address", "famsize", "Pstatus", "schoolsup", "famsup", "paid", "activities", "nursery", "higher", "internet", "Subject"]
+NMean, NGMean, NMedian, SMedian = {}, {}, {}, {}
+codes = {}
     
-print(lGMean)
-print(lMedian)
+for i in srt:
+    name = i
+    s = set(data[name].unique().tolist())
+    codes[name] = dict(zip(s, range(len(s))))
+    def code_to_num(item):
+        return codes[name][item]
+    data[name] = data[name].apply(code_to_num)
+
+for i in n+srt:
+    if i in n:
+        NMean[i] = data[i].mean()
+        NGMean[i] = np.sqrt(np.square(data[i]).mean())
+        NMedian[i] = np.median(data[i])
+    if i in srt:
+        SMedian[i] = np.median(data[i])
+    
+    
+print(SMedian)
