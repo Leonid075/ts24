4653e50deaf8c8037080d9e7a199277a3f0e590e
Psihopatch <75320638+Psihopatch@users.noreply.github.com>
2024-03-21 19:14:42 +0300

    Add files via upload

--- task4.py
+++ task4.py
@@ -4,44 +4,44 @@ import seaborn as sns
 
 data = pd.read_csv("students_data.csv")
 
-sns.catplot(data=data_update, x="G1", hue="Subject", kind="violin", bw_adjust=.6, cut=0, split=True,)
-print(data_update[data_update["Subject"] == "Por"]["G1"].value_counts())
+sns.catplot(data=data, x="G1", hue="Subject", kind="violin", bw_adjust=.6, cut=0, split=True,)
+print(data[data["Subject"] == "Por"]["G1"].value_counts())
 for i in range (6):
     try:
-        print(data_update[data_update["Subject"] == "Por"]["G1"].value_counts()[i])
+        print(data[data["Subject"] == "Por"]["G1"].value_counts()[i])
     except KeyError:
         pass
-print(data_update[data_update["Subject"] == "Math"]["G1"].value_counts())
+print(data[data["Subject"] == "Math"]["G1"].value_counts())
 for i in range (6):
     try:
-        print(data_update[data_update["Subject"] == "Math"]["G1"].value_counts()[i])
-    except KeyError:
-        pass
-    
-sns.catplot(data=data_update, x="G2", hue="Subject", kind="violin", bw_adjust=.6, cut=0, split=True,)
-print(data_update[data_update["Subject"] == "Por"]["G2"].value_counts())
-for i in range (6):
-    try:
-        print(data_update[data_update["Subject"] == "Por"]["G2"].value_counts()[i])
-    except KeyError:
-        pass
-print(data_update[data_update["Subject"] == "Math"]["G2"].value_counts())
-for i in range (6):
-    try:
-        print(data_update[data_update["Subject"] == "Math"]["G2"].value_counts()[i])
+        print(data[data["Subject"] == "Math"]["G1"].value_counts()[i])
     except KeyError:
         pass
     
-sns.catplot(data=data_update, x="G3", hue="Subject", kind="violin", bw_adjust=.6, cut=0, split=True,)
-print(data_update[data_update["Subject"] == "Por"]["G3"].value_counts())
+sns.catplot(data=data, x="G2", hue="Subject", kind="violin", bw_adjust=.6, cut=0, split=True,)
+print(data[data["Subject"] == "Por"]["G2"].value_counts())
 for i in range (6):
     try:
-        print(data_update[data_update["Subject"] == "Por"]["G3"].value_counts()[i])
+        print(data[data["Subject"] == "Por"]["G2"].value_counts()[i])
     except KeyError:
         pass
-print(data_update[data_update["Subject"] == "Math"]["G3"].value_counts())
+print(data[data["Subject"] == "Math"]["G2"].value_counts())
 for i in range (6):
     try:
-        print(data_update[data_update["Subject"] == "Math"]["G3"].value_counts()[i])
+        print(data[data["Subject"] == "Math"]["G2"].value_counts()[i])
+    except KeyError:
+        pass
+    
+sns.catplot(data=data, x="G3", hue="Subject", kind="violin", bw_adjust=.6, cut=0, split=True,)
+print(data[data["Subject"] == "Por"]["G3"].value_counts())
+for i in range (6):
+    try:
+        print(data[data["Subject"] == "Por"]["G3"].value_counts()[i])
+    except KeyError:
+        pass
+print(data[data["Subject"] == "Math"]["G3"].value_counts())
+for i in range (6):
+    try:
+        print(data[data["Subject"] == "Math"]["G3"].value_counts()[i])
     except KeyError:
         pass
