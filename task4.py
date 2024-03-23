c4e608208841e9028d554bd1d85607afda36b8d2
Psihopatch <artemij.kurdin@gmail.com>
2024-03-23 17:33:48 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,26 +1,34 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("students_data.csv")
 
-fig = sns.violinplot(data=data, x="G1", hue="Subject", bw_adjust=.6, cut=0, split=True,)
-fig = fig.get_figure()
-fig.savefig("G1.png") 
+fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(20, 3))
+axs[0].set_title("G1")
+sns.violinplot(data=data, x="G1", hue="Subject", bw_adjust=.6, cut=0, split=True, ax=axs[0])
+axs[0].grid()
 print(data[data["Subject"] == "Por"]["G1"].value_counts())
 print(data[data["Subject"] == "Math"]["G1"].value_counts())
-fig.clear()
     
-fig = sns.violinplot(data=data, x="G2", hue="Subject", bw_adjust=.6, cut=0, split=True,) 
-fig = fig.get_figure()
-fig.savefig("G2.png") 
+axs[1].set_title("G2")
+sns.violinplot(data=data, x="G2", hue="Subject", bw_adjust=.6, cut=0, split=True,ax=axs[1]) 
+axs[1].grid()
 print(data[data["Subject"] == "Por"]["G2"].value_counts())
 print(data[data["Subject"] == "Math"]["G2"].value_counts())
-fig.clear()
-    
-fig = sns.violinplot(data=data, x="G3", hue="Subject", bw_adjust=.6, cut=0, split=True,)
-fig = fig.get_figure()
-fig.savefig("G3.png") 
+
+axs[2].set_title("G3")
+sns.violinplot(data=data, x="G3", hue="Subject", bw_adjust=.6, cut=0, split=True,ax=axs[2])
+axs[2].grid()
 print(data[data["Subject"] == "Por"]["G3"].value_counts())
 print(data[data["Subject"] == "Math"]["G3"].value_counts())
-fig.clear()
+fig.tight_layout()
+fig.savefig("Grade")
+
+
+
+
+# axs[ind].set_title(i)
+# sns.boxenplot(data=data, x="G3", y="Walc", ax=axs[ind])
+# axs[ind].grid()
