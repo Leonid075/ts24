f3b3cee6740df26a422e7d7e1d60f8817ced1964
leon_id075 <grebenukleonid075@gmail.com>
2024-03-23 17:35:10 +0300

    update task4

--- task4.py
+++ task4.py
@@ -1,47 +1,32 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 data = pd.read_csv("students_data.csv")
 
-sns.catplot(data=data, x="G1", hue="Subject", kind="violin", bw_adjust=.6, cut=0, split=True,)
+fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(20, 3))
+axs[0].set_title("G1")
+sns.violinplot(data=data, x="G1", hue="Subject",
+               bw_adjust=.6, cut=0, split=True, ax=axs[0])
+axs[0].grid()
 print(data[data["Subject"] == "Por"]["G1"].value_counts())
-for i in range (6):
-    try:
-        print(data[data["Subject"] == "Por"]["G1"].value_counts()[i])
-    except KeyError:
-        pass
 print(data[data["Subject"] == "Math"]["G1"].value_counts())
-for i in range (6):
-    try:
-        print(data[data["Subject"] == "Math"]["G1"].value_counts()[i])
-    except KeyError:
-        pass
-    
-sns.catplot(data=data, x="G2", hue="Subject", kind="violin", bw_adjust=.6, cut=0, split=True,)
+
+axs[1].set_title("G2")
+sns.violinplot(data=data, x="G2", hue="Subject",
+               bw_adjust=.6, cut=0, split=True, ax=axs[1])
+axs[1].grid()
 print(data[data["Subject"] == "Por"]["G2"].value_counts())
-for i in range (6):
-    try:
-        print(data[data["Subject"] == "Por"]["G2"].value_counts()[i])
-    except KeyError:
-        pass
 print(data[data["Subject"] == "Math"]["G2"].value_counts())
-for i in range (6):
-    try:
-        print(data[data["Subject"] == "Math"]["G2"].value_counts()[i])
-    except KeyError:
-        pass
-    
-sns.catplot(data=data, x="G3", hue="Subject", kind="violin", bw_adjust=.6, cut=0, split=True,)
+
+axs[2].set_title("G3")
+sns.violinplot(data=data, x="G3", hue="Subject",
+               bw_adjust=.6, cut=0, split=True, ax=axs[2])
+axs[2].grid()
 print(data[data["Subject"] == "Por"]["G3"].value_counts())
-for i in range (6):
-    try:
-        print(data[data["Subject"] == "Por"]["G3"].value_counts()[i])
-    except KeyError:
-        pass
 print(data[data["Subject"] == "Math"]["G3"].value_counts())
-for i in range (6):
-    try:
-        print(data[data["Subject"] == "Math"]["G3"].value_counts()[i])
-    except KeyError:
-        pass
+
+fig.tight_layout()
+fig.savefig("Grade")
