42fc7e47fbc0cb53171b63a8d7a62bcde5cb5b4b
Psihopatch <artemij.kurdin@gmail.com>
2024-03-23 17:18:18 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -4,14 +4,23 @@ import seaborn as sns
 
 data = pd.read_csv("students_data.csv")
 
-sns.catplot(data=data, x="G1", hue="Subject", kind="violin", bw_adjust=.6, cut=0, split=True,).savefig("G1.png") 
+fig = sns.violinplot(data=data, x="G1", hue="Subject", bw_adjust=.6, cut=0, split=True,)
+fig = fig.get_figure()
+fig.savefig("G1.png") 
 print(data[data["Subject"] == "Por"]["G1"].value_counts())
 print(data[data["Subject"] == "Math"]["G1"].value_counts())
+fig.clear()
     
-sns.catplot(data=data, x="G2", hue="Subject", kind="violin", bw_adjust=.6, cut=0, split=True,).savefig("G2.png") 
+fig = sns.violinplot(data=data, x="G2", hue="Subject", bw_adjust=.6, cut=0, split=True,) 
+fig = fig.get_figure()
+fig.savefig("G2.png") 
 print(data[data["Subject"] == "Por"]["G2"].value_counts())
 print(data[data["Subject"] == "Math"]["G2"].value_counts())
+fig.clear()
     
-sns.catplot(data=data, x="G3", hue="Subject", kind="violin", bw_adjust=.6, cut=0, split=True,).savefig("G3.png") 
+fig = sns.violinplot(data=data, x="G3", hue="Subject", bw_adjust=.6, cut=0, split=True,)
+fig = fig.get_figure()
+fig.savefig("G3.png") 
 print(data[data["Subject"] == "Por"]["G3"].value_counts())
 print(data[data["Subject"] == "Math"]["G3"].value_counts())
+fig.clear()
